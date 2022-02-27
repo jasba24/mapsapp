24980c94bc434789c1894b544e3be9d4f39b6262
jasba24 <johanbuitrago342@gmail.com>
2022-02-26 21:30:24 -0500

    add map to the MapView component

--- src/MapsApp.tsx
+++ src/MapsApp.tsx
@@ -1,5 +1,9 @@
 import { PlacesProvider } from './context/places'
-import { Home } from './screens';
+import { Home } from './screens'
+import mapboxgl from 'mapbox-gl' // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiamFzYmEyNCIsImEiOiJjbDA0bXQwbzQxcGxqM2d0ODlmajJoaGhkIn0.wnS4402iHw-O26h7dn51JA'
 
 export const MapsApp = () => {
   return (
--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,5 +1,39 @@
+import { useContext, useLayoutEffect, useRef } from 'react'
+import { PlacesContext } from '../context/places'
+import { Loading } from '../components/Loading'
+import { Map } from 'mapbox-gl'
+
 export const MapView = () => {
+  const { isLoading, userLocation } = useContext(PlacesContext)
+  const mapDiv = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    if (!isLoading) {
+      const map = new Map({
+        container: mapDiv.current!, // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: userLocation, // starting position [lng, lat]
+        zoom: 16 // starting zoom
+      })
+    }
+  }, [isLoading])
+
+  if (isLoading) {
+    return <Loading></Loading>
+  }
+
   return (
-    <div>Aqui ira mi geolocation</div>
+    <div
+      ref={mapDiv}
+      style={{
+        height: '100vh',
+        width: '100vw',
+        position: 'fixed',
+        top: 0,
+        left: 0
+      }}
+    >
+      {userLocation?.join(', ')}
+    </div>
   )
 }
